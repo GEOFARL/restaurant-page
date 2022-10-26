5764598f3a93a30e1e51f6a1b8c5e3f22719e828
Maxim <toper.one111@gmail.com>
2022-10-26 10:21:05 +0300

    Style home page and add toggling functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.scss';
+
+const navElems = document.querySelectorAll('[data-active]');
+const pages = document.querySelectorAll('.card');
+
+navElems.forEach(elem => {
+    elem.addEventListener('click', e => {
+        navElems.forEach(elem => {
+            elem.dataset.active = "false";
+        });
+        pages.forEach(page => {
+            if (page.classList.contains(elem.dataset.page)) {
+                page.setAttribute('data-active-page', "true");
+            } else
+                page.setAttribute('data-active-page', "false");
+        })
+        e.target.dataset.active = "true";
+    })
+})
